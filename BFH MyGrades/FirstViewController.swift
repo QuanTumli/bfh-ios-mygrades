d96dbf287b847b5374ee3ab75a75555f4f3f1ea8
Jonas Mosimann <jonasmosi@gmail.com>
2016-02-25 16:21:29 +0100

    Initial Commit

---
+++ BFH MyGrades/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BFH MyGrades
+//
+//  Created by Jonas Mosimann on 25.02.16.
+//  Copyright © 2016 Percori. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
